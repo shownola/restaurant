1c39035c45f97c4d59da82651bfdfb203e2bb253
shownola <swasieko@gmail.com>
2020-12-12 13:37:02 -0700

    Add links and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products
   root 'pages#home'
-  get 'pages/catering'
-  get 'pages/info'
+  get 'catering', to: 'pages#catering'
+  get 'info', to: 'pages#info'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
